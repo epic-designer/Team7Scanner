538353d5cc8c57adc057756a2f5363faf3c15108
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-03-26 17:29:46 +0530

    Update redfunc.py

--- Team7/functions/redfunc.py
+++ Team7/functions/redfunc.py
@@ -9,3 +9,11 @@ async def passcmd_to_red(user, bancode, proof):
    else:
       scan_message = f"/scan {user.id} {bancode} {proof}"
    await assistant.send_message("Red7Systembot", scan_message)
+
+async def revert_to_red(user):
+   if user.username:
+      revert_message = f"/revert @{user.username}"
+   else:
+      revert_message = f"/revert {user.id}"
+   await assistant.send_message("Red7Systembot", revert_message)
+
