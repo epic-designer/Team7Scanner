d09dcbb185d07fc1427799dcdd74e4bab6b6d96c
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-03-26 10:24:51 +0530

    Update basic.py

--- Team7/functions/basic.py
+++ Team7/functions/basic.py
@@ -3,26 +3,28 @@ from Team7.database import users_db
 async def user_in_owners(message, user_id):
    if users_db.check_owner(user_id):
        await message.reply("User is an owner!")
-       return
+       return True
    
 async def user_in_sudos(message, user_id):
    if users_db.check_sudo(user_id):
        await message.reply("User in Sudo List!")
-       return
+       return True 
 
 async def user_in_devs(message, user_id):
    if users_db.check_dev(user_id):
        await message.reply("User in Dev List!")
-       return
+       return True
 
 async def user_in_res(message, user_id):
-   if users_db.check_owner(user_id):
-       await message.reply("User is an owner!")
+   if user_in_owners(message, user_id):
        return
-   if users_db.check_dev(user_id):
-       await message.reply("User in Dev List!")
+   if user_in_devs(message, user_id):
        return
-   if users_db.check_sudo(user_id):
-       await message.reply("User in Sudo List!")
+   if user_in_sudos(message, user_id):
        return
 
+async owner_dev(message, user_id):
+   if user_in_owners(message, user_id):
+       return
+   if user_in_devs(message, user_id):
+       return 
