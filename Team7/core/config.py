3023413748357feab8bd5e189333401611a9dc7b
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-03-26 11:00:34 +0530

    Update config.py

--- Team7/core/config.py
+++ Team7/core/config.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from Team7.functions import make_list
 
@@ -9,10 +9,40 @@ if os.path.exists(".env"):
 " ===================== CONFIGS ===================== "
 
 API_ID = int(os.getenv("API_ID", ""))
+if not API_ID:
+   print("[Team7 INFO]: You didn't fill API_ID var!")
+   sys.exit()
+else:
+   print("[Team7 INFO]: Got API_ID")
+
 API_HASH = os.getenv("API_HASH", "")
+if not API_HASH:
+   print("[Team7 INFO]: You didn't fill API_HASH var!")
+   sys.exit()
+else:
+   print("[Team7 INFO]: Got API_HASH")
+
 TOKEN = int(os.getenv("TOKEN", ""))
+if not TOKEN:
+   print("[Team7 INFO]: You didn't fill TOKEN var!")
+   sys.exit()
+else:
+   print("[Team7 INFO]: Got TOKEN")
+
 SESSION = os.getenv("SESSION", "")
+if not SESSION:
+   print("[Team7 INFO]: You didn't fill SESSION var!")
+   sys.exit()
+else:
+   print("[Team7 INFO]: Got SESSION")
+
 OWNER_IDS = make_list(os.getenv("OWNER_IDS", ""))
+if not OWNER_IDS:
+   print("[Team7 INFO]: You didn't fill OWNER_IDS var!")
+   sys.exit()
+else:
+   print("[Team7 INFO]: Got OWNER_IDS")
+
 DEVS = make_list(os.getenv("DEVS", ""))
 SUDO_USERS = make_list(os.getenv("SUDO_USERS", ""))
 SCAN_LOGS = os.getenv("SCAN_LOGS", "")
