ed89cc7addae6a3a628b10b26bcdd86b70cb7b70
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-03-26 10:54:59 +0530

    Update client.py

--- Team7/core/client.py
+++ Team7/core/client.py
@@ -1,1 +1,24 @@
+""" Team7 © RiZoeL """
 
+from pyrogram import Client
+from .config import TOKEN, SESSION
+
+print("[Team7 INFO]: Defining Scanner......")
+Team7Scanner = Client(
+    'Team7Scanner',
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=TOKEN,
+    plugins=dict(root="Team7.scanner")
+)
+print("[Team7 INFO]: Got Scanner!")
+
+print("[Team7 INFO]: Defining Assistant......")
+Team7Scanner = Client(
+    'Team7Scanner',
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=SESSION,
+    plugins=dict(root="Team7.assistant")
+)
+print("[Team7 INFO]: Got Assistant!")
