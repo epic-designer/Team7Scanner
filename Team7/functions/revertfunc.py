d0fc8eb4891ee806ba21e0e5bd0001da58cea6c1
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-03-26 16:57:58 +0530

    Update revertfunc.py

--- Team7/functions/revertfunc.py
+++ Team7/functions/revertfunc.py
@@ -6,14 +6,13 @@ from RiZoeLX.functions import delete_reply
 
 scan_cmd = """
 /revert {} 
-Reason: {}
 """
 
-async def revertpass(T7, message, user, reason):
+async def revertpass(T7, message, user):
    if user.username:
-      bancmd = scan_cmd.format(user.username, reason)
+      bancmd = scan_cmd.format(user.username)
    else:
-      bancmd = scan_cmd.format(user.id, reason)
+      bancmd = scan_cmd.format(user.id)
 
    huh = await message.reply("Passing cmd.....")
    done = 0
@@ -29,11 +28,40 @@ async def revertpass(T7, message, user, reason):
          except:
             fail += 1
 
-   final_text = f"User {user.mention} is Removed from Scanlist! \n\n cmd passed to `{done}` bots and failed in `{fail}` bots!")
+   final_text = f"User {user.mention} is Removed from Scanlist! \n\n cmd passed to `{done}` bots and failed in `{fail}` bots!"
    await delete_reply(message, huh, final_text)
-   scan_db.scan_user(user.id, reason)
+   scan_db.revert_user(user.id)
    log_msg = "**#REVERT** \n\n"
    log_msg += f"Admin: {message.from_user.mention}\n"
+   log_msg += f"User: {user.mention} (`{user.id}`) "
+   await T7.send_message(seven_logs, log_msg)
+
+
+async def revertcallpass(T7, callback, user):
+   if user.username:
+      bancmd = scan_cmd.format(user.username)
+   else:
+      bancmd = scan_cmd.format(user.id)
+
+   await callback.answer("passing cmd....")
+   done = 0
+   fail = 0
+   for bot in users_db.get_all_bots():
+      try:
+         await assistant.send_message(bot.username, bancmd)
+         done += 1
+      except:
+         try:
+            await assistant.send_message(bot.user_id, bancmd)
+            done += 1
+         except:
+            fail += 1
+   try:
+       await callback.edit_message_text(f"User {user.mention} is Removed from Scanlist! \n\n cmd passed to `{done}` bots and failed in `{fail}` bots!")
+   except Exception:
+       await callback.delete()
+   scan_db.revert_user(user.id)
+   log_msg = "**#REVERT** \n\n"
+   log_msg += f"Admin: {callback.from_user.mention}\n"
    log_msg += f"User: {user.mention} (`{user.id}`) \n"
-   log_msg += f"Reason: `{reason}` \n"
    await T7.send_message(seven_logs, log_msg)
