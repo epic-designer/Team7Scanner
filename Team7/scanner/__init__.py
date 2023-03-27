25763a549e1b60dc13087d7ad0f1927682783651
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-03-27 15:07:24 +0530

    Update __init__.py

--- Team7/scanner/__init__.py
+++ Team7/scanner/__init__.py
@@ -1,1 +1,8 @@
 
+from Team7.functions import import_members, import_owners, import_devs, import_sudos 
+
+
+Team7Users = import_members()
+Owners = import_owners() 
+Devs = import_devs()
+Sudos = import_sudos()
