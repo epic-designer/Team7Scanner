5914547055f82f3de22aafee9f4d886265911e8e
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-03-27 14:30:21 +0530

    Update basic.py

--- Team7/functions/basic.py
+++ Team7/functions/basic.py
@@ -1,5 +1,12 @@
 from Team7.database import users_db 
+from Team7 import Team7Scanner
 
+
+async def itsme(message, user_id):
+    me = await Team7Scanner.get_me()
+    id user_id == me.id:
+       await message.reply("Noob 😑 It's mee!")
+       return True
 
 async def user_in_owners(message, user_id):
    if users_db.check_owner(user_id):
@@ -22,6 +29,8 @@ async def user_in_res(message, user_id):
    if user_in_devs(message, user_id):
        return True
    if user_in_sudos(message, user_id):
+       return True
+   if itsme(message, user_id):
        return True
 
 async def owner_dev(message, user_id):
