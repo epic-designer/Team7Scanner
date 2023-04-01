dea67d306fcc8a0ac0b1bcb613d2ea5337d82cf2
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-02 01:09:42 +0530

    Update scanfunc.py

--- Team7/functions/scanfunc.py
+++ Team7/functions/scanfunc.py
@@ -67,6 +67,42 @@ async def scanpass(T7, message, user, reason_code, proof):
       print(f"{user.first_name} is Noob!")
       pass
 
+async def scancallpass(T7, callback, user, reason_code, proof):
+   reason, red7code = await check_reason(reason_code)
+   if user.username:
+      bancmd = scan_cmd.format(user.username, reason, proof)
+   else:
+      bancmd = scan_cmd.format(user.id, reason, proof)
+
+   await callback.answer("passing cmd....")
+   done = 0
+   fail = 0
+   for bot in users_db.get_all_bots():
+      try:
+         await assistant.send_message(bot.username, bancmd)
+         done += 1
+      except:
+         try:
+            await assistant.send_message(bot.user_id, bancmd)
+            done += 1
+         except:
+            fail += 1
+   try:
+       await callback.edit_message_text(f"User {user.mention} is now in Scanlist! \n\n cmd passed to `{done}` bots and failed in `{fail}` bots!")
+   except Exception:
+       await callback.delete()
+   scan_db.scan_user(user.id, reason_code)
+   try:
+      await passcmd_to_red(user, red7code, proof)
+   except Exception as eor:
+      print(f"[Team7 Error]: {str(eor)}")
+      pass
+   log_msg = "**#SCAN** \n\n"
+   log_msg += f"Admin: {callback.from_user.mention}\n"
+   log_msg += f"User: {user.mention} (`{user.id}`) \n"
+   log_msg += f"Reason: `{reason}` \n"
+   log_msg += f"Proof: `{proof}` \n"
+   await T7.send_message(seven_logs, log_msg)
 
 @Team7Scanner.on_callback_query(filters.regex(r'revert'))
 async def scan_callback(T7: Team7Scanner, callback: CallbackQuery):
