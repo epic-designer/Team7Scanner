0b2de788dee5886c572bfc8f54150f65dbc7ade9
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-02 01:10:36 +0530

    Update callbacks.py

--- Team7/scanner/callbacks.py
+++ Team7/scanner/callbacks.py
@@ -1,6 +1,7 @@
 """ © Team7 RiZoeL """
 
 from Team7.database import users_db
+from Team7.funtions import report_user_query
 
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
@@ -11,7 +12,9 @@ async def T7callbacks(T7: Client, callback_query: CallbackQuery):
    chat = callback_query.message.chat
    admin = callback_query.from_user
    message = callback_query.message
-   if query == "reject_report":
+   if query == "do_repor":
+      await report_user_query(T7, message)
+   elif query == "reject_report":
       if users_db.check_owner(admin.id) or users_db.check_dev(admin.id):
          try:
              await callback_query.edit_message_text(f"**Report Rejected By admin {admin.mention}!**")
