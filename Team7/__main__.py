b85b75ecf7316ed5e49ea76c51d970b7d527359b
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-02 15:08:39 +0530

    Update __main__.py

--- Team7/__main__.py
+++ Team7/__main__.py
@@ -1,4 +1,20 @@
 
 import os, sys
 from Team7.core import *
+from Team7.database import users_db
+
+print("[Team7 INFO]: Adding users in db!")
+for user_id in OWNER_IDS:
+   user_db.add_owner(user_id)
+
+print("[Team7 INFO]: Adding Devs in DB")
+for user_id in DEVS:
+   user_db.add_dev(user_id)
+print("[Team7 INFO]: Added all devs in DB")
+print("[Team7 INFO]: Moving to next steps!")
+
+if __name__ == "__main__":
+   Start_assistant()
+   StartScanner()
+
 
