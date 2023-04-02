759a1c549b5c9d097e5cbb5e54475000fb88437f
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-02 16:59:09 +0530

    Update __main__.py

--- Team7/__main__.py
+++ Team7/__main__.py
@@ -5,11 +5,11 @@ from Team7.database import users_db
 
 print("[Team7 INFO]: Adding users in db!")
 for user_id in OWNER_IDS:
-   user_db.add_owner(user_id)
+   users_db.add_owner(user_id)
 
 print("[Team7 INFO]: Adding Devs in DB")
 for user_id in DEVS:
-   user_db.add_dev(user_id)
+   users_db.add_dev(user_id)
 print("[Team7 INFO]: Added all devs in DB")
 print("[Team7 INFO]: Moving to next steps!")
 
