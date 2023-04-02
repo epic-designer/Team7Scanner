542251ee8e364954f2cc804b8678558e3f3a19a9
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-02 13:40:47 +0530

    Update __init__.py

--- Team7/database/__init__.py
+++ Team7/database/__init__.py
@@ -1,1 +1,17 @@
+from seven import DB_URL
+import sqlalchemy
+#from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
 
+
+def start() -> scoped_session:
+    engine = sqlalchemy.create_engine(DB_URL, client_encoding="utf8")
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+SESSION = start()
