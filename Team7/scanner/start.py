a7211f92c568ba4e896a1c4af098ae48d93ee2d6
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-02 10:30:53 +0530

    Update start.py

--- Team7/scanner/start.py
+++ Team7/scanner/start.py
@@ -2,7 +2,7 @@
 
 from pyrogram import Clients, filters
 from pyrogram.types import Message, InlineKeyboardMarkup
-from Team7.core import start_msg, start_buttons, about_msg, about_buttons, help_msg, alive_buttons
+from Team7.core import start_msg, alive_buttons
 
 PIC = ""
 GRP_PIC = ""
@@ -12,7 +12,7 @@ async def start_(_, message: Message):
     if message.from_user.id == message.chat.id:
        msg = start_msg.format(message.from_user.mention)
        try:
-         await message.reply_photo(PIC, caption=msg, reply_markup=InlineKeyboardMarkup(start_buttons))
+         await message.reply_photo(PIC, caption=msg, reply_markup=InlineKeyboardMarkup(alive_buttons))
        except:
          await message.reply_text(msg, reply_markup=InlineKeyboardMarkup(start_buttons))
     else:
@@ -20,13 +20,3 @@ async def start_(_, message: Message):
          await message.reply_video(GRP_PIC, caption="Contact me in PM to start :) ")
        except:
          await message.reply_text("Contact me in PM to start :) ")
-
-
-@Client.on_message(filters.command(["help"], ["!", "/"]))
-async def help_(_, message: Message):
-    await message.reply(help_msg, reply_markup=InlineKeyboardMarkup(alive_buttons))
-
-@Client.on_message(filters.command(["about"], ["!", "/"]))
-async def about_(_, message: Message):
-   if message.from_user.id == message.chat.id:
-      await message.reply(about_msg, reply_markup=InlineKeyboardMarkup(about_buttons))
