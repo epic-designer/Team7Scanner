12b7c981311a9bc93737d40ad60df9485a310e4a
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-02 16:04:47 +0530

    Update __init__.py

--- Team7/__init__.py
+++ Team7/__init__.py
@@ -4,7 +4,5 @@ print("[Team7 INFO]: ----Team7 Scanner Here----")
 print("[Team7 INFO]: starting process.......")
 
 import time, datetime, sys, os
-from Team7.core import *
-from Team7.functions import *
 
 start_time = time.time()
