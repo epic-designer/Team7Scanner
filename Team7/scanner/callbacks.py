e46a7816a9f0bc0d23f210c2d7d0000fd7445743
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-02 19:15:28 +0530

    Update callbacks.py

--- Team7/scanner/callbacks.py
+++ Team7/scanner/callbacks.py
@@ -4,7 +4,7 @@ import time, datetime
 from Team7 import start_time
 from Team7.database import users_db
 from Team7.functions import report_user_query, get_time
-from Team7.core import alive_msg, get_stats
+from Team7.core import inline_alive_msg, get_stats
 
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
@@ -35,10 +35,10 @@ async def T7callbacks(T7: Client, callback_query: CallbackQuery):
       uptime = await get_time((time.time() - start_time))
       end = datetime.datetime.now()
       ms = (end-start).microseconds / 1000
-      await callback_query.answer(f"**Team7 Scanner Here!** \n\n Ping: {ms}ms \n Uptime: {uptime}", show_alert=True)
+      await callback_query.answer(f"Team7 Scanner Here!\n\n Ping: {ms}ms \n Uptime: {uptime}", show_alert=True)
 
    elif query == "alive":
-      await callback_query.answer(alive_msg, show_alert=True)
+      await callback_query.answer(inline_alive_msg, show_alert=True)
 
    elif query == "stats":
       await callback_query.answer(get_stats(), show_alert=True)
