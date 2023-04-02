f4fca9aa3cfc29189e92b205b0c5a672093909ee
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-02 10:50:56 +0530

    Update start.py

--- Team7/scanner/start.py
+++ Team7/scanner/start.py
@@ -2,7 +2,7 @@
 
 from pyrogram import Clients, filters
 from pyrogram.types import Message, InlineKeyboardMarkup
-from Team7.core import start_msg, alive_buttons
+from Team7.core import start_msg, start_buttons
 
 PIC = ""
 GRP_PIC = ""
@@ -12,9 +12,9 @@ async def start_(_, message: Message):
     if message.from_user.id == message.chat.id:
        msg = start_msg.format(message.from_user.mention)
        try:
-         await message.reply_photo(PIC, caption=msg, reply_markup=InlineKeyboardMarkup(alive_buttons))
+         await message.reply_photo(PIC, caption=msg, reply_markup=InlineKeyboardMarkup(start_buttons))
        except:
-         await message.reply_text(msg, reply_markup=InlineKeyboardMarkup(alive_buttons))
+         await message.reply_text(msg, reply_markup=InlineKeyboardMarkup(start_buttons))
     else:
        try:
          await message.reply_video(GRP_PIC, caption="Contact me in PM to start :) ")
