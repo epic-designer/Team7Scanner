371098f3c028cb58952dfebb26a88c1bbc7f563a
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-02 16:07:12 +0530

    Update __init__.py

--- Team7/database/__init__.py
+++ Team7/database/__init__.py
@@ -1,10 +1,18 @@
-from Team7.core import DB_URL
-import sqlalchemy
+#from Team7.core import DB_URL
+import sqlalchemy, os, sys
 #from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 
+DB_URL = os.getenv("DATABASE_URL", "")
+if DB_URL:
+   print("[Team7 INFO]: Got DATABASE_URL")
+   if 'postgres' in DB_URL and 'postgresql' not in DB_URL:
+      DB_URL = DB_URL.replace("postgres", "postgresql")
+else:
+   print("[Team7 INFO]: You didn't fill DATABASE_URL")
+   sys.exit()
 
 def start() -> scoped_session:
     engine = sqlalchemy.create_engine(DB_URL, client_encoding="utf8")
