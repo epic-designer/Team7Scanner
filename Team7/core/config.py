e7f0b42dc03291e58d9b605a1cdb282af1608f85
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-02 16:46:43 +0530

    Update config.py

--- Team7/core/config.py
+++ Team7/core/config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 if os.path.exists(".env"):
     load_dotenv(".env")
 
-""Make list""
 def make_list(str_input):
     str_list = str_input.split(" ")
     int_list = []
