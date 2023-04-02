ce2e1a4b531360cb09be58df8f2bcfe172d9a18f
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-02 13:05:43 +0530

    Update __init__.py

--- Team7/functions/__init__.py
+++ Team7/functions/__init__.py
@@ -1,1 +1,11 @@
+""" All in One! """
+
+from .basic import *
+from .check_reason import *
+from .getuser import *
+from .redfunc import *
+from .reportfunc import *
+from .redfunc import *
+from .scanfunc import *
+from .utils import *
 
