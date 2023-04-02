0c73119d55c7bd1a042ba59d01791d35abdb4511
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-02 18:51:46 +0530

    Update callbacks.py

--- Team7/scanner/callbacks.py
+++ Team7/scanner/callbacks.py
@@ -3,7 +3,7 @@ import time, datetime
 
 from Team7 import start_time
 from Team7.database import users_db
-from Team7.funtions import report_user_query, get_time
+from Team7.functions import report_user_query, get_time
 from Team7.core import alive_msg, get_stats
 
 from pyrogram import Client, filters
