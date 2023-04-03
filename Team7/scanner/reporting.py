0eac15088333de34763bad43384424bf524ba6b7
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-03 17:20:47 +0530

    Update reporting.py

--- Team7/scanner/reporting.py
+++ Team7/scanner/reporting.py
@@ -3,8 +3,8 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from Team7.database import report_db, users_db as db
-from Team7.functions import report_user_query, getuser
-from . import Owners, Devs 
+from Team7.functions import report_user_query, getuser, user_in_res
+from . import Owners, Devs
 
 
 def sudo_checking(user_id):
