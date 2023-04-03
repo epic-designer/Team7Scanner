17430793ff60031a4a39255fb5349145a03bb498
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-03 12:08:00 +0530

    Update basic.py

--- Team7/functions/basic.py
+++ Team7/functions/basic.py
@@ -43,12 +43,16 @@ async def owner_dev(message: Message, user_id):
        return True 
 
 async def make_tg(message):
+   await message.reply("downloading media...")
    doc = await message.download()
    try:
       media_url = upload_file(doc)
+      tg_url = f"https://telegra.ph/{media_url[0]}"
+      os.remove(doc)
+      await message.delete()
+      return tg_url
    except exceptions.TelegraphException as exc:
       await message.reply(f"**ERROR:** `{exc}`")
       os.remove(doc)
-      return
-   return f"https://telegra.ph/{media_url[0]}"
-   os.remove(doc)
+      return 
+   
