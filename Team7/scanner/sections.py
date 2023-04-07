acb9873880750bec464725055853083d9a123964
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-07 17:40:35 +0530

    Update sections.py

--- Team7/scanner/sections.py
+++ Team7/scanner/sections.py
@@ -73,3 +73,60 @@ async def add_bot(T7: Client, message: Message):
 
    else:
       await message.reply(f"{extra.mention} is not bot!")
+
+
+@Client.on_message(filters.user(Owners) & filters.command(["rmdev"], ["!", "?", "/", "."]))
+async def rmdev(T7: Client, message: Message):
+   user = await getuser(T7, message)
+   if not db.check_dev(user.id):
+      await message.reply("user not in dev list!")
+      return
+
+   db.rm_dev(user.id)
+   await message.reply(f"User {user.mention} is successfully removed from Dev ⚜️ -!")
+
+   Logs = "**#REMOVE DEV**\n\n"
+   Logs += f"**× Admin:** {message.from_user.mention} (`{message.from_user.id}`)\n"
+   Logs += f"**× User:** {user.mention} (`{user.id}`)"
+   await T7.send_message(SEVEN_LOGS, Logs)
+
+
+@Client.on_message(filters.user(Owners) & filters.command(["rmsudo"], ["!", "?", "/", "."]))
+@Client.on_message(filters.user(Devs) & filters.command(["rmsudo"], ["!", "?", "/", "."]))
+async def rmuser(T7: Client, message: Message):
+   user = await getuser(T7, message)
+   check = db.check_sudo(user.id)
+   if check:
+      bot = await T7.get_users(check.username)
+      db.rm_bot(bot.id)
+      db.rm_sudo(user.id)
+      await message.reply_text(f"User {user.mention} is successfully removed from sudo list 👤-!")
+      Logs = "**#REMOVE SUDO**\n\n"
+      Logs += f"**× Admin:** {message.from_user.mention} (`{message.from_user.id}`)\n"
+      Logs += f"**× User:** {user.mention} (`{user.id}`) \n"
+      await T7.send_message(SEVEN_LOGS, Logs)
+
+   else:
+      await message.reply("user not in sudo list!")
+      return
+
+
+@Client.on_message(filters.user(Owners) & filters.command(["addbot"], ["!", "?", "/", "."]))
+@Client.on_message(filters.user(Devs) & filters.command(["addbot"], ["!", "?", "/", "."]))
+async def rm_bot(T7: Client, message: Message):
+   bot = await getuser(T7, message)
+   if await itsme(message, bot.id):
+      return
+   if bot.is_bot:
+      if not db.check_bot(bot.id):
+         await message.reply(f"@{bot.username} not in bot list!")
+         return
+      db.rm_bot(bot.id)
+      await message.reply(f"User {bot.mention} is successfully removed from bot list 🤖 -!")
+      Logs = "**#REMOVE BOT**\n\n"
+      Logs += f"**× Admin:** {message.from_user.mention} (`{message.from_user.id}`)\n"
+      Logs += f"**× Bot:** @{bot.username} (`{bot.id}`)"
+      await T7.send_message(SEVEN_LOGS, Logs)
+
+   else:
+      await message.reply(f"{extra.mention} is not bot!")
